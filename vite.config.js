881afcbaae5bd0eb1de86b8e10d26d1378f7ca75
SeedCats <s928641007@gmail.com>
2025-09-05 23:44:40 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,52 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    
+    VitePWA({
+      registerType: 'autoUpdate',  // Automatically updates the service worker
+      devOptions: { enabled: true },  // Enables PWA in development mode for testing
+      includeAssets: ['vite.svg'],  // Assets to cache
+      injectRegister: 'auto',  // Automatically injects the service worker registration
+
+      manifest: {
+        id: '/',
+        name: 'My Vue PWA',
+        short_name: 'VuePWA',
+        description: 'A Progressive Web App built with Vue and Vite',
+        start_url: '/',
+        scope: '/',
+        display: 'standalone',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+
+        icons: [
+          {
+            src: 'icon192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ],
+
+        "screenshots": [
+          {
+            "src": "screenshot.png",
+            "sizes": "1600x800",
+            "type": "image/png",
+            "form_factor": "wide",
+            "label": "Homepage"
+          },
+        ]
+      }
+
+    })
+  ]
+});
